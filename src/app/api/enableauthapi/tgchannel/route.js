6a91a1f0e7c1285388f3a9d177572aec6c9839c4
xiao8hello <xiao8hello@gmail.com>
2025-09-14 12:57:07 +0800

    Update route.js

--- src/app/api/enableauthapi/tgchannel/route.js
+++ src/app/api/enableauthapi/tgchannel/route.js
@@ -57,7 +57,7 @@ export async function POST(request) {
 		const res_img = await fetch(up_url, {
 			method: "POST",
 			headers: {
-				"User-Agent": " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+				"User-Agent": " Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:142.0) Gecko/20100101 Firefox/142.0"
 			},
 			body: newformData,
 		});
