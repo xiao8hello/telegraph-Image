9688f1a30725b87fd60927fe1c3b416e34b305f2
xiao8hello <xiao8hello@gmail.com>
2025-09-15 19:50:40 +0800

    Update route.js
    修改了 GET 函数中的 responseHeaders 对象，移除了 Content-Disposition 这一行。浏览器现在会根据 Content-Type 来判断如何处理文件，从而实现直接预览。

--- src/app/api/cfile/[name]/route.js
+++ src/app/api/cfile/[name]/route.js
@@ -120,7 +120,7 @@ export async function GET(request, { params }) {
 
         const contentType = getContentType(fileName);
         const responseHeaders = {
-          "Content-Disposition": `attachment; filename=${fileName}`,
+          //"Content-Disposition": `attachment; filename=${fileName}`,
           "Access-Control-Allow-Origin": "*",
           "Content-Type": contentType
         };
